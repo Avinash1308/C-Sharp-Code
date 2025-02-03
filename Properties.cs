cf5368b40dd2003269f573e3e19d55402b37ed51
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2025-02-03 21:21:02 +0530

    Create Properties.cs

---
+++ Properties.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+
+namespace LearnC_
+{
+    class Learn
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+    }
+}
+
+//program class to call object of Learn class
+using System;
+
+
+namespace LearnC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Learn learn = new Learn();
+            learn.Name = "Avinash";
+            Console.WriteLine(learn.Name);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
