ff949ea962531b06bc5aae14bd09b56fb922279c
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2025-02-03 11:36:34 +0530

    Create Typecasting.cs

---
+++ Typecasting.cs
@@ -0,0 +1,21 @@
+using System;
+namespace program{
+  class Program{
+      public static void Main(String[] args){ 
+//Implicit Casting
+     int x=5;
+     double y= x;
+
+      Console.WriteLine(x);
+      Console.WriteLine(y);  
+//Explicit Casting
+double a=3.5;
+int b= (int) a;
+
+
+      Console.WriteLine(a);
+      Console.WriteLine(b);
+
+    }
+  }
+}
