9e988d0b70f1e64b372c48ab56d08e50149382e8
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2025-02-03 12:54:44 +0530

    Create Interpolation.cs

---
+++ Interpolation.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Program{
+  class Program{
+    public static void Main(String[] args){
+        String fname="Avinash";
+        String lname = "Kedar";
+        String fullname= $"My fullname is {fname} {lname}";
+        Console.WriteLine(fullname);
+    }
+  }
+}
