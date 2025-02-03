376435aa991570fa42406a6d95a33c5cea043067
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2025-02-03 11:44:57 +0530

    Update Typecasting.cs

--- Typecasting.cs
+++ Typecasting.cs
@@ -19,3 +19,23 @@ int b= (int) a;
     }
   }
 }
+
+//using default type convertion methods
+using System;
+namespace program{
+  class Program{
+      public static void Main(String[] args){ 
+        int a=2;
+        double b=3.5;
+        
+        bool val=false;
+
+        Console.WriteLine(Convert.ToString(a));
+        Console.WriteLine(Convert.ToDouble(a));
+        Console.WriteLine(Convert.ToInt32(b));
+        Console.WriteLine(Convert.ToString(val));
+        
+        
+    }
+  }
+}
