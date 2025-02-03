5e1048a5006a71f06fe051ab4799b2863a3a3786
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2025-02-03 16:08:29 +0530

    Create ArrayIntializer.cs

---
+++ ArrayIntializer.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnC_
+{
+    class Learn
+    {
+        public void Learning()
+        {
+            string[] cars = { "volvo", "mercedes", "bmw" };
+            Console.WriteLine(cars[0]);
+
+            string[] bikes = new string[4];
+
+            string[] cycles = new string[4] { "c1", "c2","c3","C4"};
+
+
+        }
+    }
+}
